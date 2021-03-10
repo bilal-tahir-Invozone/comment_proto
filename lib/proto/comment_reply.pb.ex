e288cf7bfe428aa04759d4ff3af4c3855b8759b9
bilal-tahir-Invozone <bilal.tahir@invozone.com>
2021-03-10 15:55:48 +0500

    proto_inti

---
+++ lib/proto/comment_reply.pb.ex
@@ -0,0 +1,311 @@
+defmodule Commentapi.UpdateReplyRequest do
+  @moduledoc false
+  use Protobuf, syntax: :proto3
+
+  @type t :: %__MODULE__{
+          replyid: integer
+        }
+
+  defstruct [:replyid]
+
+  field :replyid, 1, type: :int32
+end
+
+defmodule Commentapi.UpdateReplyResponse do
+  @moduledoc false
+  use Protobuf, syntax: :proto3
+
+  @type t :: %__MODULE__{
+          status: boolean
+        }
+
+  defstruct [:status]
+
+  field :status, 1, type: :bool
+end
+
+defmodule Commentapi.DeleteReplyResponse do
+  @moduledoc false
+  use Protobuf, syntax: :proto3
+
+  @type t :: %__MODULE__{
+          status: boolean
+        }
+
+  defstruct [:status]
+
+  field :status, 1, type: :bool
+end
+
+defmodule Commentapi.DeleteReplyRequest do
+  @moduledoc false
+  use Protobuf, syntax: :proto3
+
+  @type t :: %__MODULE__{
+          replyid: integer
+        }
+
+  defstruct [:replyid]
+
+  field :replyid, 1, type: :int32
+end
+
+defmodule Commentapi.GetReplyRequest do
+  @moduledoc false
+  use Protobuf, syntax: :proto3
+
+  @type t :: %__MODULE__{
+          replyid: integer
+        }
+
+  defstruct [:replyid]
+
+  field :replyid, 1, type: :int32
+end
+
+defmodule Commentapi.CreateReplyRequest do
+  @moduledoc false
+  use Protobuf, syntax: :proto3
+
+  @type t :: %__MODULE__{
+          reply: String.t(),
+          isdeletedbyadmin: boolean,
+          userid: integer,
+          replyid: integer,
+          postid: integer,
+          status: integer,
+          likecount: integer,
+          userlikes: integer,
+          commentid: integer
+        }
+
+  defstruct [
+    :reply,
+    :isdeletedbyadmin,
+    :userid,
+    :replyid,
+    :postid,
+    :status,
+    :likecount,
+    :userlikes,
+    :commentid
+  ]
+
+  field :reply, 1, type: :string
+  field :isdeletedbyadmin, 2, type: :bool
+  field :userid, 3, type: :int32
+  field :replyid, 4, type: :int32
+  field :postid, 5, type: :int32
+  field :status, 6, type: :int32
+  field :likecount, 7, type: :int32
+  field :userlikes, 8, type: :int32
+  field :commentid, 9, type: :int32
+end
+
+defmodule Commentapi.CreateReplyResponse do
+  @moduledoc false
+  use Protobuf, syntax: :proto3
+
+  @type t :: %__MODULE__{
+          reply: String.t(),
+          isdeletedbyadmin: boolean,
+          userid: integer,
+          replyid: integer,
+          postid: integer,
+          status: integer,
+          likecount: integer,
+          userlikes: integer,
+          commentid: integer
+        }
+
+  defstruct [
+    :reply,
+    :isdeletedbyadmin,
+    :userid,
+    :replyid,
+    :postid,
+    :status,
+    :likecount,
+    :userlikes,
+    :commentid
+  ]
+
+  field :reply, 1, type: :string
+  field :isdeletedbyadmin, 2, type: :bool
+  field :userid, 3, type: :int32
+  field :replyid, 4, type: :int32
+  field :postid, 5, type: :int32
+  field :status, 6, type: :int32
+  field :likecount, 7, type: :int32
+  field :userlikes, 8, type: :int32
+  field :commentid, 9, type: :int32
+end
+
+defmodule Commentapi.UpdateCommentRequest do
+  @moduledoc false
+  use Protobuf, syntax: :proto3
+
+  @type t :: %__MODULE__{
+          commentid: integer
+        }
+
+  defstruct [:commentid]
+
+  field :commentid, 1, type: :int32
+end
+
+defmodule Commentapi.UpdateCommentResponse do
+  @moduledoc false
+  use Protobuf, syntax: :proto3
+
+  @type t :: %__MODULE__{
+          status: boolean
+        }
+
+  defstruct [:status]
+
+  field :status, 1, type: :bool
+end
+
+defmodule Commentapi.DeleteCommentResponse do
+  @moduledoc false
+  use Protobuf, syntax: :proto3
+
+  @type t :: %__MODULE__{
+          status: boolean
+        }
+
+  defstruct [:status]
+
+  field :status, 1, type: :bool
+end
+
+defmodule Commentapi.DeleteCommentRequest do
+  @moduledoc false
+  use Protobuf, syntax: :proto3
+
+  @type t :: %__MODULE__{
+          commentid: integer
+        }
+
+  defstruct [:commentid]
+
+  field :commentid, 1, type: :int32
+end
+
+defmodule Commentapi.GetCommentRequest do
+  @moduledoc false
+  use Protobuf, syntax: :proto3
+
+  @type t :: %__MODULE__{
+          commentid: integer
+        }
+
+  defstruct [:commentid]
+
+  field :commentid, 1, type: :int32
+end
+
+defmodule Commentapi.CreateCommentRequest do
+  @moduledoc false
+  use Protobuf, syntax: :proto3
+
+  @type t :: %__MODULE__{
+          comment: String.t(),
+          isdeletedbyadmin: boolean,
+          userid: integer,
+          replyid: integer,
+          postid: integer,
+          status: integer,
+          likecount: integer,
+          userlikes: integer,
+          commentid: integer
+        }
+
+  defstruct [
+    :comment,
+    :isdeletedbyadmin,
+    :userid,
+    :replyid,
+    :postid,
+    :status,
+    :likecount,
+    :userlikes,
+    :commentid
+  ]
+
+  field :comment, 1, type: :string
+  field :isdeletedbyadmin, 2, type: :bool
+  field :userid, 3, type: :int32
+  field :replyid, 4, type: :int32
+  field :postid, 5, type: :int32
+  field :status, 6, type: :int32
+  field :likecount, 7, type: :int32
+  field :userlikes, 8, type: :int32
+  field :commentid, 9, type: :int32
+end
+
+defmodule Commentapi.CreateCommentResponse do
+  @moduledoc false
+  use Protobuf, syntax: :proto3
+
+  @type t :: %__MODULE__{
+          comment: String.t(),
+          isdeletedbyadmin: boolean,
+          userid: integer,
+          replyid: integer,
+          postid: integer,
+          status: integer,
+          likecount: integer,
+          userlikes: integer,
+          commentid: integer
+        }
+
+  defstruct [
+    :comment,
+    :isdeletedbyadmin,
+    :userid,
+    :replyid,
+    :postid,
+    :status,
+    :likecount,
+    :userlikes,
+    :commentid
+  ]
+
+  field :comment, 1, type: :string
+  field :isdeletedbyadmin, 2, type: :bool
+  field :userid, 3, type: :int32
+  field :replyid, 4, type: :int32
+  field :postid, 5, type: :int32
+  field :status, 6, type: :int32
+  field :likecount, 7, type: :int32
+  field :userlikes, 8, type: :int32
+  field :commentid, 9, type: :int32
+end
+
+defmodule Commentapi.Comment.Service do
+  @moduledoc false
+  use GRPC.Service, name: "commentapi.Comment"
+
+  rpc :CreateComment, Commentapi.CreateCommentRequest, Commentapi.CreateCommentResponse
+
+  rpc :GetComment, Commentapi.GetCommentRequest, Commentapi.CreateCommentResponse
+
+  rpc :DeleteComment, Commentapi.DeleteCommentRequest, Commentapi.DeleteCommentResponse
+
+  rpc :updateComment, Commentapi.UpdateCommentRequest, Commentapi.UpdateCommentResponse
+
+  rpc :CreateReply, Commentapi.CreateReplyRequest, Commentapi.CreateReplyResponse
+
+  rpc :GetReply, Commentapi.GetReplyRequest, Commentapi.CreateReplyResponse
+
+  rpc :DeleteReply, Commentapi.DeleteReplyRequest, Commentapi.DeleteReplyResponse
+
+  rpc :updateReply, Commentapi.UpdateReplyRequest, Commentapi.UpdateReplyResponse
+end
+
+defmodule Commentapi.Comment.Stub do
+  @moduledoc false
+  use GRPC.Stub, service: Commentapi.Comment.Service
+end
---
+++ mix.exs
@@ -0,0 +1,30 @@
+defmodule Proto.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :proto,
+      version: "0.1.0",
+      elixir: "~> 1.11",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application, do: [applications: [:grpc]]
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:protobuf, "~> 0.7.1"},
+      {:grpc, github: "elixir-grpc/grpc"},
+      # 2.9.0 fixes some important bugs, so it's better to use ~> 2.9.0
+      {:cowlib, "~> 2.9.0", override: true},
+      {:pokemon_proto, github: "castengo/elixirconf_pokemon_proto"},
+      {:google_protos, "~> 0.1"}
+      # {:dep_from_hexpm, "~> 0.3.0"},
+      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+    ]
+  end
+end
